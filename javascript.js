a740a4943808be97bbe4a2c32c60c6df1a4b0e32
Adam Taber <altaber@gmail.com>
2022-05-21 19:59:44 -0500

    Add on-screen text and modal

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+const modalOne = document.getElementById("modalOne");
+const playAgainOne = document.getElementById("playAgainOne");
+
+
 let computerRandom = [
     "rock",
     "paper",
@@ -11,7 +15,7 @@ function getRandom() {
 
 let playerInput;
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button.inputBtn');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playerInput = button.id;
@@ -21,6 +25,23 @@ buttons.forEach((button) => {
     });
 });
 
+const restartButtons = document.querySelectorAll('button.playAgain');
+restartButtons.forEach((e) => {
+    e.addEventListener('click', () => {
+        window.location.reload();
+    })
+})
+
+modalOne.addEventListener('click', function(event) {
+    let content = document.getElementById("contentBox");
+    if(event.target != content) {
+        window.location.reload();
+    }
+},
+);
+
+
+
 
 let computerScore = 0;
 let playerScore = 0;
@@ -29,46 +50,62 @@ function playRound() {
     const computerSelection = getRandom();
     const playerSelection = playerInput;
     if(computerSelection === playerInput) {
-        alert(`It's a tie!`);
+        document.getElementById('results').textContent =`It's a tie!`;
     }else if((playerInput === "rock") && computerSelection === "scissors") {
         ++playerScore;
-        alert(`You Win! ${playerInput} beats ${computerSelection}`);
+        document.getElementById('results').textContent =
+        `You Win! ${playerInput} beats ${computerSelection}`;
     }else if((playerSelection === "paper") && computerSelection === "rock") {
         ++playerScore;
-        alert(`You Win! ${playerInput} beats ${computerSelection}`);    
+        document.getElementById('results').textContent =
+        `You Win! ${playerInput} beats ${computerSelection}`;    
     }else if ((playerSelection === "scissors") && computerSelection === "paper") {
         ++playerScore;
-        alert(`You Win! ${playerInput} beats ${computerSelection}`);    
+        document.getElementById('results').textContent =
+        `You Win! ${playerInput} beats ${computerSelection}`;    
     }else {
         ++computerScore;
-        alert(`You Lose! ${computerSelection} beats ${playerInput}`);
+        document.getElementById('results').textContent =
+        `You Lose! ${computerSelection} beats ${playerInput}`;
     }
+    document.getElementById('playerScore').textContent = `${playerScore}`;
+    document.getElementById('computerScore').textContent = `${computerScore}`;
 }
 
 function winAnnounce() {
     while(computerScore === 5 || playerScore === 5) {
         if(computerScore === playerScore) {
-            document.getElementById('results').textContent = 
-            "GAME OVER: IT'S A TIE";
-            computerScore = 0;
-            playerScore = 0;
+            document.getElementById('resultsText').textContent = "It's a Tie!";
+            announce();
             break;
         }else if(computerScore > playerScore) {
-            document.getElementById('results').textContent =
-            "GAME OVER: YOU LOSE";
-            computerScore = 0;
-            playerScore = 0;
-            break;
+            document.getElementById('resultsText').textContent =
+             "You're the overall loser!";
+             announce();
+             break;
         }else if(playerScore > computerScore) {
-            document.getElementById('results').textContent =
-            "GAME OVER: YOU WIN!";
-            computerScore = 0;
-            playerScore = 0;
-            break;
+            document.getElementById('resultsText').textContent =
+             "You're the overall winner!";
+             announce();
+             break;
+        }
         }
     }
+
+function announce() {
+    modalOne.style.display = "block";
 }
 
+
+
+/*buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerInput = button.id;
+        console.log(playerInput);
+        playRound();
+        winAnnounce();
+    });
+});*/
 // player hits r/p/s
 // when player hits button, playRound is run
 // once computerScore or playerScore hits 5, announce win/loss
